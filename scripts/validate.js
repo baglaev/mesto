3b490a79ffc170469986532844d7cc50a74fad4c
Sasha Baglaev <bglvssh@yandex.ru>
2023-03-21 22:54:50 +0300

    feat: webinar 1:14

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,24 +1,49 @@
-const setEventListeners = (profileFormVal, inputList) => {
-    profileForm.addEventListener('submit', (event) => {
+const showInputError = (errorTextElement, validationMessage, activeErrorClass) => {
+    errorTextElement.textContent = validationMessage;
+    errorTextElement.classList.add(activeErrorClass);
+}
+
+const hideInputError = (errorTextElement, activeErrorClass) => {
+    errorTextElement.classList.remove(activeErrorClass);
+}
+
+const checkInputValidity = (input, errorClassTemplate, activeErrorClass) => {
+    const errorTextElement = document.querySelector(`${errorClassTemplate}${input.name}`)
+    console.log(errorTextElement)
+    if (input.validity.valid) {
+        showInputError(errorTextElement, input.validationMessage, activeErrorClass);
+       
+    } else {
+        hideInputError(errorTextElement);
+        
+    }
+}
+
+const setEventListeners = (profileFormVal, inputList, errorClassTemplate, activeErrorClass) => {
+    profileFormVal.addEventListener('submit', (event) => {
         event.preventDefault();
     });
     
     inputList.forEach((input) => {
         input.addEventListener('input', (event) => {
-            console.log(event.target.value)
+            checkInputValidity(input, errorClassTemplate, activeErrorClass);
         });
     });
 }
 
 
-const enableValudation = () => {
-    const popupCardVal = document.querySelector('.popup-card');
-    const profileFormVal = popupCardVal.querySelector('.popup__form');
-    const inputList = profileFormVal.querySelectorAll('.popup__input');
+const enableValudation = (config) => {
+    const popupCardVal = document.querySelector(config.popupCardSelector);
+    const profileFormVal = popupCardVal.querySelector(config.formSelector);
+    const inputList = profileFormVal.querySelectorAll(config.inputSelector);
     
-    setEventListeners(profileFormVal, inputList);
+    setEventListeners(profileFormVal, inputList, config.errorClassTemplate, config.activeErrorClass);
 }
 
 enableValudation({
-    
+    popupCardSelector: '.popup-card',
+    formSelector: '.popup__form',
+    inputSelector: '.popup__input',
+    errorClassTemplate: '.popup__input-error_type_',
+    activeErrorClass: 'popup__input-error'
 });
